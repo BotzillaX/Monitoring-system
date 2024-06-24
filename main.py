2d488cd1233769944509d2dfa49ec60f7ce4fb04
Kevin Fritsch <103073906+BotzillaX@users.noreply.github.com>
2024-06-24 15:02:56 +0200

    added datetime

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from getInfo import ramUsage
 from time import sleep
+import datetime 
 
 
 while True:
+    current_datetime = datetime.datetime.today()
+    time_and_date_str = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
+    print(time_and_date_str)
     sleep(0.1)
     ramUsage()
