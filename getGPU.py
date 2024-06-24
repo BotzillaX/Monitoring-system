d76cc3e164e1641cac21955498ca18380316a35e
Kevin Fritsch <103073906+BotzillaX@users.noreply.github.com>
2024-06-24 16:43:31 +0200

    change gpu

--- getGPU.py
+++ getGPU.py
@@ -1,30 +1,23 @@
-import psutil
 import GPUtil
-from tabulate import tabulate
 import time
 
-def get_gpu_info():
+def get_gpu_load():
     gpus = GPUtil.getGPUs()
-    gpu_list = []
-    for gpu in gpus:
-        gpu_info = {
-            "GPU": gpu.name,
-            "Load (%)": f"{gpu.load * 100:.2f}",
-            "Memory Used (MB)": f"{gpu.memoryUsed:.2f}",
-            "Memory Total (MB)": f"{gpu.memoryTotal:.2f}",
-            "Temperature (C)": f"{gpu.temperature:.2f}",
-        }
-        gpu_list.append(gpu_info)
-    return gpu_list
+    if gpus:
+        gpu_load = gpus[0].load * 100
+        return gpu_load
+    else:
+        return None
 
-def print_gpu_info():
-    gpu_info = get_gpu_info()
-    if gpu_info:
-        print(tabulate(gpu_info, headers="keys"))
+def print_gpu_load():
+    gpu_load = get_gpu_load()
+    if gpu_load is not None:
+        print(f"Current GPU Load: {gpu_load:.2f}%")
     else:
         print("No GPU found.")
 
 if __name__ == "__main__":
     while True:
         print("\n" + "="*30 + " GPU Usage " + "="*30)
-        print_gpu_info()
+        print_gpu_load()
+        time.sleep(1)  # Sleep for 1 second before printing the next reading
